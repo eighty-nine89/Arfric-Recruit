c1645194ed35bd0b9501c81d753944b964249f77
e-hokage777 <e.hokage777@gmail.com>
2023-12-02 21:27:38 +0000

    updated styling

--- views/regional.php
+++ views/regional.php
@@ -5,8 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../assets/css/general.css">
     <title>Regional monitor</title>
+    <style>
+        body{
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
 </head>
 <body>
-    <h3 style="text-align: center; margin-top:25%;">Hello there, welcome to Reginal Monitor!</h3>
+    <h3>Hello there, welcome to Reginal Monitor!</h3>
 </body>
 </html>
